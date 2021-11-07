be1998da5b8b06808ba1b0c0ea3cd55bf531a175
Sinan Aksu <sinanksu@gmail.com>
2021-11-07 09:40:12 +0300

    refactor: utils/http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
-class Http {
-  init() {
-    return axios.create({
-      baseURL: process.env.API_URL,
-    })
-  }
-}
+const req = axios.create({
+  baseURL: process.env.API_URL,
+})
 
-export default new Http()
+export default req
