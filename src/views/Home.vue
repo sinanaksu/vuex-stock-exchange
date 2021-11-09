8192c8cbe02b23a72bbfb482bbfaec2489b230bf
Sinan Aksu <sinanksu@gmail.com>
2021-11-09 17:34:33 +0300

    feat: add Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <TheHeader />
+
+
+</div>
+</template>
+
+<script>
+import { mapGetters, mapState, mapActions, mapMutations } from 'vuex';
+
+import TheHeader from "../components/TheHeader";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      text: "",
+      symbol: this.$route.params.symbol,
+    }
+  },
+  components: {
+    TheHeader
+  },
+  computed: {
+		...mapGetters(['searchTypeMovie', 'favNumber']),
+		...mapState(['pageType','selectSymbol']),
+	},
+  methods: {
+  ...mapActions(['searchSymbol','csvtest']),
+  ...mapMutations(['SET_PAGE_TYPE', 'SET_SEARCH_DATA','SET_SEARCH_DATA']),
+  }
+};
+</script>
