712685e814f9b049c28c9626eb8328d65a7880e4
Sinan Aksu <sinanksu@gmail.com>
2021-11-11 21:18:07 +0300

    feat: add home and chart route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Chart from "../views/Chart.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {},
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+  path: '/symbol/:symbol',
+  name: 'Symbol Price Chart',
+  component: Chart },
 ];
 
 const router = new VueRouter({
